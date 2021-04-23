f39fb687c7eb38e6e7e021b6c5fed0a174745e82
sydney <sydneychase5841@gmail.com>
2021-04-23 12:10:47 -0400

    testing fecth func creating div

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@
 const BASE_URL = "https://api.lyrics.ovh";
 
 function getLyrics() {
-    const url = `${BASE_URL}/v1/:artist/:title`;
+    const url = `${BASE_URL}/v1/Janet Jackson/control`;
     return fetch(url)
         .then(res => res.json())
    
 }
 // console.log to see if it returns an error 404 which it should with object error no lyrics found. It did
-// The promise is working correctly
+// The promise is working correctly . console log with one artist and one song to see if
+// lyrics were retrieved during console.log they were. 2 tests passed. 
 const lyrics = getLyrics();
 console.log(lyrics);
 getLyrics().then(data => console.log(data));
+
+
+// create test div for Janet Jackson data
+/* <div class="lyrics" id="lyrics"> 
+</div> */
+
+
+  
 
