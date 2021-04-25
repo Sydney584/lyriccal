324dc30d024ade928fd0dafce172143fed098bee
sydney <sydneychase5841@gmail.com>
2021-04-25 00:20:25 -0400

    Event Listener test with console.log

--- src/index.js
+++ src/index.js
@@ -14,52 +14,44 @@ function getArtistandSong(artist, title) {
      
   };
   
+// Creating an Event Listner for the form
+form.addEventListener('submit', e => {
+  e.preventDefault();
 
+  const searchTerm = search.value.trim();
+  console.log(searchTerm);
 
-function getArtistandSong() {
+});
+
+// function getArtistandSong() {
  
     
-    return fetch(url)
-        .then(res => res.json()).then(console.log)
+//     return fetch(url)
+//         .then(res => res.json()).then(console.log)
    
-}
-// console.log to see if it returns an error 404 which it should with object error no lyrics found. It did
-// The promise is working correctly . console log with one artist and one song to see if
-// lyrics were retrieved during console.log they were. 2 tests passed. 
-const lyrics = getLyrics();
-console.log();
-getArtistandSong().then(data => console.log(data));
+// }
+// // console.log to see if it returns an error 404 which it should with object error no lyrics found. It did
+// // The promise is working correctly . console log with one artist and one song to see if
+// // lyrics were retrieved during console.log they were. 2 tests passed. 
+// const lyrics = getLyrics();
+// console.log();
+// getArtistandSong().then(data => console.log(data));
 
 
-// create test div for Janet Jackson data
-/* <div id="lyrics"> 
-</div> */
+// // create test div for Janet Jackson data
+// /* <div id="lyrics"> 
+// </div> */
 
-function createLyricsDiv(lyricsObj) {
-const div = document.createElement('div'),
-p = document.createElement('p');
+// function createLyricsDiv(lyricsObj) {
+// const div = document.createElement('div'),
+// p = document.createElement('p');
 
-p.textContent = lyricsObj.lyrics;
+// p.textContent = lyricsObj.lyrics;
 
-div.appendChild(p);
+// div.appendChild(p);
 
-return div;
+// return div;
 
-}
+// }
 
-
-
-
-function postlyrics(lyrics) {
-  let URL = `${BASE_URL}/v1/Janet Jackson/control`,
-    config = {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-        "Accept": "application/json",
-      },
-      body: JSON.stringify(lyrics),
-    };
-
-  fetch(URL, config);
-}
+// // 
